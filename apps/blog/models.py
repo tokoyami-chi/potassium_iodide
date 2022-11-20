51965b407df5f884f24138fe2d1106acc0915574
tokoyami_chi <liza2008belaa@gmail.com>
2022-11-20 17:37:44 +0200

    blog part 1

---
+++ apps/blog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class BlogCategory(models.Model):
+    name = models.CharField(verbose_name='Назва категорії', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категорія блогу'
+        verbose_name_plural = 'Категорії блогу'
+
+
+class Article(models.Model):
+    category = models.ForeignKey(to=BlogCategory, verbose_name='Категорія', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Назва', max_length=255)
+    text_preview = models.TextField(verbose_name='Превью тексту', null=True, blank=True)
+    text = models.TextField(verbose_name='Текст')
+    publish_date = models.DateTimeField(verbose_name='Дата публікації')
+    updatet_at = models.DateTimeField(verbose_name='Дата зміни', auto_now=True)
+    created_at = models.DateTimeField(verbose_name='Дата створення', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Стаття'
+        verbose_name_plural = 'Статті'
