ba6317f8831e902fb488c861939e49a55055c97c
tokoyami_chi <liza2008belaa@gmail.com>
2022-11-22 09:38:22 +0200

    Revert "blog Tag's part"
    This reverts commit e5d9cd510218595c157cf17197a078de5f340bf0.

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from apps.blog.models import BlogCategory, Article, Tag
+from apps.blog.models import BlogCategory, Article
 
 
 admin.site.register(BlogCategory)
 admin.site.register(Article)
-admin.site.register(Tag)
